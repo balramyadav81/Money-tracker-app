b136960043b5bc4ea7fd3e148addaff431a44159
balramyadav81 <yadavbalram934@gmail.com>
2024-05-22 22:34:34 +0530

    final commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+
+import './App.css';
+import Navbar from './components/Navbar';
+import Dashboard from './components/Dashboard';
+
+function App() {
+  return (
+    <>
+    <div className="flex">
+      {/* Side Navigation Bar */}
+      <Navbar />
+      {/* Main component on basis of selected navigation from nav bar */}
+      <main className="grow">
+        <Dashboard />
+      </main>
+    </div>
+  </>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,109 @@
+import React, { useState, useEffect } from "react";
+import Logo from "../assets/Logo.png";
+import {
+  ArrowLeftRightIcon,
+  BarChart3Icon,
+  Clock4Icon,
+  LayoutDashboard,
+  HelpCircleIcon,
+} from "lucide-react";
+import { motion } from "framer-motion";
+
+import RightArrowIcon from "../assets/icon/rightArrow.svg";
+
+const variants = {
+  // todo: change expanded to 30% and nonexpanded to %6
+  expanded: { width: "220px" },
+  nonexpanded: { width: "60px" },
+};
+
+const navLinks = [
+  {
+    link: "Dashboard",
+    icon: LayoutDashboard,
+  },
+  {
+    link: "Activity",
+    icon: Clock4Icon,
+  },
+  {
+    link: "Analytics",
+    icon: BarChart3Icon,
+  },
+  {
+    link: "Transactions",
+    icon: ArrowLeftRightIcon,
+  },
+  {
+    link: "Support",
+    icon: HelpCircleIcon,
+  },
+];
+
+function Navbar() {
+  // todo: change to true
+  const [isExpanded, setIsExpanded] = useState(true);
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  // screen resize
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+      if (windowWidth < 768) {
+        setIsExpanded(false);
+      }
+    };
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  return (
+    <motion.div
+      animate={isExpanded ? "expanded" : "nonexpanded"}
+      variants={variants}
+      className={
+        "py-10 h-screen flex flex-col border border-r-1 bg-[#FDFDFD] relative" +
+        (isExpanded ? " px-10" : " px-2 duration-500")
+      }
+    >
+      <div
+        onClick={() => setIsExpanded(!isExpanded)}
+        className="cursor-pointer absolute -right-3 top-10 rounded-full w-6 h-6 bg-[#FF8C8C] md:flex hidden justify-center items-center"
+      >
+        <img src={RightArrowIcon} className="w-2" />
+      </div>
+
+      <div className="logo-div flex space-x-4 items-center">
+        <img src={Logo} className="md:w-6 w-4 ml-2" />
+        <span className={!isExpanded ? "hidden" : "block"}>Money Tracker</span>
+      </div>
+
+      <div className="flex flex-col space-y-8 mt-12">
+        {navLinks.map((item, index) => (
+          <div className="nav-links w-full" key={index}>
+            <div
+              onClick={() => setActiveIndex(index)}
+              className={
+                "flex space-x-3 w-full p-2 rounded " +
+                (activeIndex === index
+                  ? "bg-[#FF8C8C] text-white"
+                  : " text-black") +
+                (!isExpanded ? " pl-3" : "")
+              }
+            >
+              <item.icon className="md:w-6 w-4" />
+              <span className={!isExpanded ? "hidden" : "block"}>
+                {item.link}
+              </span>
+            </div>
+          </div>
+        ))}
+      </div>
+    </motion.div>
+  );
+}
+
+export default Navbar;
